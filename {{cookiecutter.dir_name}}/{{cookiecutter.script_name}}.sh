5f524d95f84ad5dcbfca485637764e3afa06115d
TjWallas <tjwallas@cryptolab.net>
2017-06-26 15:55:26 +0200

    Initial commit

---
+++ {{cookiecutter.dir_name}}/{{cookiecutter.script_name}}.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# https://sipb.mit.edu/doc/safe-shell/
+# GLOBBING IS NOT ALLOWED!!
+set -euf -o pipefail
+
+# https://stackoverflow.com/questions/2853803/in-a-shell-script-echo-shell-commands-as-they-are-executed
+set -x
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+IFS=$'\n\t'
+
+# Code begins here...
